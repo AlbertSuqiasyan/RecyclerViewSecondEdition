765a6128b2f4910070a3b58016005f2c6952c8e9
Anteroxc <53874213+Anteroxc@users.noreply.github.com>
2020-01-18 18:17:21 +0400

    fixed some stuff

--- app/src/main/java/com/example/recyclerviewhomework/ResultAdapter.kt
+++ app/src/main/java/com/example/recyclerviewhomework/ResultAdapter.kt
@@ -1,9 +1,11 @@
 package com.example.recyclerviewhomework
 
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 
-class ResultAdapter(private val list: List<Result>,val clickListener: ResultListener) : RecyclerView.Adapter<ResultViewHolder>() {
+class ResultAdapter(private val list: List<Result>,val clickListener: ResultListener) : ListAdapter<Result,ResultViewHolder>(ResultDiffCallback(list)) {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ResultViewHolder {
         return ResultViewHolder.from(parent)
     }
@@ -23,13 +25,13 @@ class ResultAdapter(private val list: List<Result>,val clickListener: ResultList
 
 
 // try diffutil again later
-//class ResultDiffCallback(private val list: List<Result>) : DiffUtil.ItemCallback<Result>() {
-//    override fun areItemsTheSame(oldItem: Result, newItem: Result): Boolean {
-//        return oldItem.webTitle == newItem.webTitle
-//    }
-//
-//    override fun areContentsTheSame(oldItem: Result, newItem: Result): Boolean {
-//        return oldItem == newItem
-//    }
-//
-//}
+class ResultDiffCallback(private val list: List<Result>) : DiffUtil.ItemCallback<Result>() {
+    override fun areItemsTheSame(oldItem: Result, newItem: Result): Boolean {
+        return oldItem.webTitle == newItem.webTitle
+    }
+
+    override fun areContentsTheSame(oldItem: Result, newItem: Result): Boolean {
+        return oldItem == newItem
+    }
+
+}
--- app/src/main/java/com/example/recyclerviewhomework/ResultListener.kt
+++ app/src/main/java/com/example/recyclerviewhomework/ResultListener.kt
@@ -1,5 +1,2 @@
 package com.example.recyclerviewhomework
 
-class ResultListener(val clickListener: (resultId: String) -> Unit){
-    fun onClick(result: Result) = clickListener(result.id)
-}
